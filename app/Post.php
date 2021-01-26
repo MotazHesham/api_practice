091baed6392af570c0e9f889aa4c234452c85e14
nada-farid <nodyfared6@gmail.com>
2021-01-26 21:28:07 +0200

    complete task

--- app/Post.php
+++ app/Post.php
@@ -7,13 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'author_id',
+        'title', 'description', 'image', 'author_id'
     ];
+
+    protected $hidden=['created_at','updated_at'];
 
     //each post has many comments belongs to many users
     public function users()
     {
         return $this->belongsToMany(User::class,'comments')->withpivot('comment');
+    }
+    ///////ندا 
+    public function comment()
+    {
+        return $this->hasMany(Comment::class,'post_id','id');
     }
 
     //each post belongs to one author
